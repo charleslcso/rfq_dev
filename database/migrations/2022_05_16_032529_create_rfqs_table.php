94a155b5dc6dc5ef92152aa03dfc9ea19fd722d5
juliusyip <wwx026118@gmail.com>
2022-05-16 11:30:39 +0800

    add migration file for rfq

--- database/migrations/2022_05_16_032529_create_rfqs_table.php
+++ database/migrations/2022_05_16_032529_create_rfqs_table.php
@@ -14,7 +14,21 @@ class CreateRfqsTable extends Migration
     public function up()
     {
         Schema::create('rfqs', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->string('telephone');
+            $table->string('email', 100);
+            $table->string('company_name', 100);
+            $table->string('company_address', 200);
+            $table->string('project_name', 100);
+            $table->boolean('is_tvp');
+            $table->text('project_requirements');
+            $table->string('expected_budget', 100);
+            $table->text('notes');
+            $table->integer('number_of_rfqs');
+            $table->string('payment_received');
+            $table->string('ip');
+            $table->text('stripe_data');
             $table->timestamps();
         });
     }
